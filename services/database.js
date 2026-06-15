1c480ae901552a16f9091cb0fef1222443561562
Andyka Triyono Putra <andyka.triyono@plnindonesiapower.co.id>
2026-06-16 05:38:00 +0700

    Change default page to login

--- services/database.js
+++ services/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    host: process.env.MYHOST || process.env.MYSQLHOST,
+    user: process.env.MYUSER || process.env.MYSQLUSER,
+    password: process.env.MYPASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.MYDATABASE || process.env.MYSQLDATABASE,
+    port: process.env.MYPORT || process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10
 });
